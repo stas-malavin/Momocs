07104f6c4ee48313cc10f84bbaf4b3632eca0ccd
stas-malavin <stas.malavin@gmail.com>
2017-02-25 22:11:39 +0200

    plot.PCA: add loadings.mult parameter

--- R/gr-PCA.R
+++ R/gr-PCA.R
@@ -62,6 +62,7 @@
 #' @param n.kde2d the number of bins for \link{kde2d}, ie the 'smoothness' of density kernel
 #' @param delaunay logical whether to add a delaunay 'mesh' between points
 #' @param loadings logical whether to add loadings for every variables
+#' @param loadings.mult scalar multiplication factor for loadings' arrows
 #' @param labelspoints if TRUE rownames are used as labels, a colname from $fac can also be passed
 #' @param col.labelspoints a color for these labels, otherwise inherited from fac
 #' @param cex.labelspoints a cex for these labels
@@ -388,7 +389,7 @@ plot.PCA <- function(x, fac, xax=1, yax=2,
            col=col.labelspoints, cex=cex.labelspoints)
     }
   }
-  if (loadings)   .loadings(PCA$rotation[, c(xax, yax)], d = 2)
+  if (loadings)   .loadings(PCA$rotation[, c(xax, yax)], d = )
   if (axisnames)  .axisnames(xax, yax, "PC")
   if (axisvar)    .axisvar(PCA$sdev, xax, yax)
   if (unit)       .unit(nb.grids)
